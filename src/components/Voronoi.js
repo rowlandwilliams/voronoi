457bafee672cee1567c345273c9b3532503635bd
Rowland Williams <rowlandwilliams@Rowlands-MBP.lan>
2021-05-09 18:56:45 +0100

    add ref and plotpoint function

--- src/components/Voronoi.js
+++ src/components/Voronoi.js
@@ -1,3 +1,6 @@
+import { useEffect, useRef } from "react";
+import * as d3 from "d3";
+
 function generateCoords(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -7,16 +10,29 @@ function Voronoi() {
   for (var i = 0; i < 1000; i++) {
     dataset.push([generateCoords(0, 100) + "%", generateCoords(0, 100) + "%"]);
   }
-  console.log(generateCoords(0, 100));
+  console.log(dataset);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    plotPoints();
+  }, [dataset]);
+
+  const plotPoints = () => {
+    const svgElement = d3.select(ref.current);
+    svgElement
+      .selectAll("circle")
+      .data(dataset)
+      .join("circle")
+      .attr("cx", (d) => d[0])
+      .attr("cy", (d) => d[1])
+      .attr("r", 3)
+      .attr("fill", "blue");
+  };
 
   return (
     <div style={{ margin: "10px", boxSizing: "border-box" }}>
-      <svg width="100%" height="calc(100vh - 20px)">
-        {dataset.map((x) => (
-          <circle cx={x[0]} cy={x[1]} r="3px" fill="blue" />
-        ))}
-        {/* <rect width="100%" height="100%" fill="red"></rect> */}
-      </svg>
+      <svg width="100%" height="calc(100vh - 20px)" ref={ref}></svg>
     </div>
   );
 }
