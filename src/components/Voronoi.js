d10c90e57744833a15eb71578828dcc1ca60d115
Rowland Williams <rowlandwilliams@Rowlands-MBP.lan>
2021-05-10 12:53:36 +0100

    initial voronoi in voroni

--- src/components/Voronoi.js
+++ src/components/Voronoi.js
@@ -28,27 +28,36 @@ function Voronoi() {
   const color = colorSchemes[Math.floor(Math.random() * colorSchemes.length)];
 
   // define dataset
-  var nCells = 100;
-  var sites = d3.range(nCells).map(function (d) {
-    return [Math.random() * width, Math.random() * height];
-  });
+  var initialCells = 80;
+  // var nCells = 100;
 
+  const generatePoints = (nCells, startX, endX, startY, endY) =>
+    d3.range(nCells).map(() => {
+      const posWidth = endX - startX;
+      const posHeight = endY - startY;
+      return [
+        startX + Math.random() * posWidth,
+        startY + Math.random() * posHeight,
+      ];
+    });
+
+  var sites = generatePoints(initialCells, 0, width, 0, height);
   const ref = useRef();
 
   useEffect(() => {
-    plotPoints();
-    window.addEventListener("resize", plotPoints());
+    plot();
+    window.addEventListener("resize", plot());
   }, [sites]);
 
-  const plotPoints = () => {
+  const plot = () => {
     d3.select(".svg").remove();
 
     const svg = d3
       .select(ref.current)
       .append("svg")
       .attr("class", "svg")
-      .attr("viewBox", [0, 0, width, height])
-      .on("mousemove", moved);
+      .attr("viewBox", [0, 0, width, height]);
+    // .on("mousemove", moved);
 
     var generateVoronoi = voronoi()
       .x((d) => d[0])
@@ -58,7 +67,7 @@ function Voronoi() {
         [width, height],
       ]);
 
-    var polygon = svg
+    var intial = svg
       .append("g")
       .attr("class", "voronoi")
       .selectAll("path")
@@ -69,15 +78,58 @@ function Voronoi() {
       .attr("stroke", "#333333")
       .call(redrawPolygon);
 
-    function moved(event) {
-      sites[0] = d3.pointer(event);
-      redraw();
-    }
+    var polygons = generateVoronoi(sites).polygons();
 
-    function redraw() {
-      var diagram = generateVoronoi(sites);
-      polygon = polygon.data(diagram.polygons()).call(redrawPolygon);
-    }
+    // [min.x, max.x, min.y, max.y]
+
+    polygons = polygons.map((x) => x.filter((y) => y !== "data"));
+
+    polygons.forEach((x, i) => {
+      const bounds = [
+        Math.min(...x.map((y) => y[0])),
+        Math.max(...x.map((y) => y[0])),
+        Math.min(...x.map((y) => y[1])),
+        Math.max(...x.map((y) => y[1])),
+      ];
+
+      // generate new points
+      const level2 = generatePoints(
+        10,
+        bounds[0],
+        bounds[1],
+        bounds[2],
+        bounds[3]
+      );
+      console.log(bounds);
+
+      var test = svg.append("g").attr("class", "suh" + i);
+
+      test
+        .selectAll("circle")
+        .data(level2)
+        .join("circle")
+        .attr("cx", (d) => d[0])
+        .attr("cy", (d) => d[1])
+        .attr("r", 3)
+        .attr("fill", "blue");
+
+      var generateVoronoi2 = voronoi()
+        .x((d) => d[0])
+        .y((d) => d[1])
+        .extent([
+          [0, 0],
+          [bounds[1], bounds[3]],
+        ]);
+
+      test
+        .selectAll("path")
+        .data(generateVoronoi2(sites).polygons())
+        .enter()
+        .append("path")
+        .attr("fill", (d, i) => color(Math.random()))
+        .attr("stroke", "#333333")
+        .call(redrawPolygon);
+    });
 
     function redrawPolygon(polygon) {
       polygon.attr("d", function (d) {
