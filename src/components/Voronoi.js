21daf2e49e581f9858b5e10aa8fd1282b8f85609
Rowland Williams <rowlandwilliams@Rowlands-MBP.lan>
2021-05-10 11:18:23 +0100

    remove link / sites functions

--- src/components/Voronoi.js
+++ src/components/Voronoi.js
@@ -6,28 +6,35 @@ import {
   interpolatePlasma,
   interpolateTurbo,
   interpolateCool,
+  interpolateInferno,
+  interpolateYlOrRd,
 } from "d3";
 
 function Voronoi() {
   const width = window.innerWidth;
   const height = window.innerHeight;
+
+  // generate array of all color schemes
   const colorSchemes = [
     interpolatePlasma,
     interpolateRainbow,
     interpolateTurbo,
     interpolateCool,
+    interpolateInferno,
+    interpolateYlOrRd,
   ];
-  const color = colorSchemes[Math.floor(Math.random() * colorSchemes.length)];
-  console.log(color);
-  var nCells = 100;
 
+  // set color scheme each time
+  const color = colorSchemes[Math.floor(Math.random() * colorSchemes.length)];
+
+  // define dataset
+  var nCells = 100;
   var sites = d3.range(nCells).map(function (d) {
     return [Math.random() * width, Math.random() * height];
   });
 
   const ref = useRef();
 
-  // const colorScheme =
   useEffect(() => {
     plotPoints();
     window.addEventListener("resize", plotPoints());
@@ -67,24 +74,6 @@ function Voronoi() {
       .attr("stroke", "#333333")
       .call(redrawPolygon);
 
-    var link = svg
-      .append("g")
-      .attr("class", "links")
-      .selectAll("line")
-      .data(generateVoronoi(sites).links())
-      .enter()
-      .append("line")
-      .attr("stroke", "blue");
-    // .call(redrawLink);
-
-    var site = svg
-      .selectAll("circle")
-      .data(sites)
-      .join("circle")
-      .attr("r", 1)
-      .attr("fill", "white");
-    // .call(redrawSite);
-
     function moved(event) {
       sites[0] = d3.pointer(event);
       redraw();
@@ -93,42 +82,12 @@ function Voronoi() {
     function redraw() {
       var diagram = generateVoronoi(sites);
       polygon = polygon.data(diagram.polygons()).call(redrawPolygon);
-      // link = link.data(diagram.links());
-      // link.exit().remove();
-      // link = link.enter().append("line").merge(link).call(redrawLink);
-      // site = site.data(sites).call(redrawSite);
     }
 
     function redrawPolygon(polygon) {
       polygon.attr("d", function (d) {
         return d ? "M" + d.join("L") + "Z" : null;
       });
-    }
-
-    function redrawLink(link) {
-      link
-        .attr("x1", function (d) {
-          return d.source[0];
-        })
-        .attr("y1", function (d) {
-          return d.source[1];
-        })
-        .attr("x2", function (d) {
-          return d.target[0];
-        })
-        .attr("y2", function (d) {
-          return d.target[1];
-        });
-    }
-
-    function redrawSite(site) {
-      site
-        .attr("cx", function (d) {
-          return d[0];
-        })
-        .attr("cy", function (d) {
-          return d[1];
-        });
     }
   };
 
